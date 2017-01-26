1dce7796c7b0049a9c55f38a051e3bddb430021a
Sen <markjansen17@gmail.com>
2017-01-27 01:08:23 +0800

    Login

---
+++ app/src/main/java/eventcoordinator2017/myevent/ui/events/EventsAdapter.java
@@ -0,0 +1,8 @@
+package eventcoordinator2017.myevent.ui.events;
+
+/**
+ * Created by Sen on 1/26/2017.
+ */
+
+public class EventsAdapter {
+}
---
+++ app/src/main/java/eventcoordinator2017/myevent/ui/events/add/EventAddPresenter.java
@@ -0,0 +1,12 @@
+package eventcoordinator2017.myevent.ui.events.add;
+
+import com.hannesdorfmann.mosby.mvp.MvpNullObjectBasePresenter;
+
+/**
+ * Created by Mark Jansen Calderon on 1/26/2017.
+ */
+
+public class EventAddPresenter extends MvpNullObjectBasePresenter<EventAddView> {
+
+
+}
